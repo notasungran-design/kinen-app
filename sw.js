2635896952f8f297fa784e73fb23848ddfc29c70
notasungran-design <n.ota.sungran@gmail.com>
2026-09-25 11:58:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // オフライン対応：まずネットから最新を取り、つながらないときは保存しておいたものを使う
-const CACHE = 'kinen-v2';
+const CACHE = 'kinen-v3';
 const FILES = ['./', 'index.html', 'manifest.json', 'icon-180.png', 'icon-192.png', 'icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
